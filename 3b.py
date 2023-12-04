8a549ccc1e4c23803c9c13ffcec2f4d466ee6f6b
Leonardo-Boss <70913810+Leonardo-Boss@users.noreply.github.com>
2023-12-03 21:48:47 -0300

    day 3 completed

--- 3b.py
+++ 3b.py
@@ -1,28 +1,34 @@
 import re
 
-with open('tests/3a') as f:
+with open('inputs/3a') as f:
     data = f.readlines()
-
-
-def issymbol(c):
-    return not(c.isdigit() or c == '.' or c=='\n')
 
 lxd = len(data[0])
 lyd = len(data)
 indexes = (-1,0,1)
 indexes = tuple(tuple((i,j) for j in indexes) for i in indexes)
-parts = 0
 
+gears = {}
 for i, line in enumerate(data):
-    for numbers in re.finditer(r'\*', line):
-        j = numbers.start()
-        adjecent_nums=[]
-        for row in indexes:
-            for x,y in row:
-                n = i+x
-                m = j+y
-                if m<=0 and n<=0 or m>=lxd or n>=lyd: continue
-                if data[n][m].isdigit(): adjecent_nums.append(data[n][m])
-        if len(adjecent_nums)==2:
-            parts += adjecent_nums[0]*adjecent_nums[1]
-print(parts)
+    for numbers in re.finditer(r'\d+', line):
+        for j in range(numbers.start(), numbers.end()):
+            ispart=False
+            for row in indexes:
+                for x,y in row:
+                    n = i+x
+                    m = j+y
+                    if m<=0 and n<=0 or m>=lxd or n>=lyd: continue
+                    if data[n][m] == '*':
+                        gears[f'{n}-{m}']= (p if (p:=gears.get(f'{n}-{m}')) else []) + [int(numbers.group(0))]
+                        break
+                else: continue
+                break
+            else:continue
+            break
+
+s=0
+for i in gears.values():
+    print(i)
+    if len(i) == 2:
+        s+=i[0]*i[1]
+print(s)
