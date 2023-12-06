b47004479727c7b341d48462f18d02906d4138f6
Leonardo Boss <70913810+Leonardo-Boss@users.noreply.github.com>
2023-12-06 12:18:49 -0300

    day 5 incomplete

--- 5b.py
+++ 5b.py
@@ -20,13 +20,32 @@ for i in data[1:]:
         i[2] = i[1]+i[2]
         transformations[key] = transformations.get(key, []) + [i]
 
-results = []
-for seed in seeds:
-    trans = seed
-    for key, transformation in transformations.items():
+transformations = list(transformations.items())
+
+transformers = []
+def get_transf(tranformations):
+    if len(transformations) == 1:
+        pass
+    t = []
+    for destiny1, start1, end1 in transformations[0]:
+        for destiny2, start2, end2 in transformations[1]:
+            array = ((start1, destiny1, True),
+                    (start2, destiny2, True),
+                    (end1, destiny1, False),
+                    (end2, destiny2, False),)
+            s = sorted(array, key=lambda x: x[0])
+
+            # startsmall - start bigger/ endsmall
+            ds = start1 - start2
+            de = end2 - end1
+            if start1 >= start2 and end1 < end2:
+                t.append(destiny1-start2+destiny2)
+
+
+for destiny, start, end in first:
+    for key, transformation in transformations:
         for destiny, start, end in transformation:
             if trans < start or trans >= end: continue
             trans = trans-start+destiny
             break
-    results.append(trans)
 print(min(results))
